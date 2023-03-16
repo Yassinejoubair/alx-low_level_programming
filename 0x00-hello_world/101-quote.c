ce17bfa577814430be3d0f55607cd5548c9ffc0f
Yassine Joubair <yassinejoubair00@gmail.com>
2023-03-16 12:13:01 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+ * main print
+ * Return : always:1
+ * */
+int main (void)
+{
+	fput("and that piece of art is useful"" - Dora Korpar," 201"5-10-19");
+}	
