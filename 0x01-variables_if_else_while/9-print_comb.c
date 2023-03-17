9a6ce41fab1ed820ff9b12d7698e16ca4c752769
Yassine Joubair <yassinejoubair00@gmail.com>
2023-03-17 12:15:48 -0700

    comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ * main - Entry point
+ * 
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i ;
+
+	for(i = 0 ; i < 10 , i++)
+	{
+		putchar(i + '0');
+		if(i<9)
+		{
+			putchar(,);
+			putchar('');
+		}
+		putchar('\n');
+	}
+	return (0);
