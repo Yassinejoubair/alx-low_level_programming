39fb5ec37b517f4d07192a3ed7005ee391fdb131
Yassine Joubair <yassinejoubair00@gmail.com>
2023-03-17 11:08:24 -0700

    Reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ *  main - Entry point
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char alphab;
+		
+	for (alphab = 'a'; alphab >= 'z'; alphab--)
+	{
+		putchar(alphab);
+	}
+	putchar('\n');
+	return (0);
+}
