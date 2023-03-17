92ae29c9f710b45e64333167a5704655c7dc73a8
Yassine Joubair <yassinejoubair00@gmail.com>
2023-03-17 09:35:07 -0700

    print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include<stdlib.h>
+#include<stdio.h>
+/* main - Entry point
+ *
+ * Return: 0 on success
+ *
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z'; ++ch)
+	{
+		if ((ch == 'q' || ch == 'e') !== 0)
+		{
+			putchar(ch);
+		}
+	}
+	return (0);
