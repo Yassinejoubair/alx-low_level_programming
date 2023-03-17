c2bdcb2e34aacddabe5afbfd39bcb9807536d151
Yassine Joubair <yassinejoubair00@gmail.com>
2023-03-17 09:48:41 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+nclude <stdlib.h>
+#include <stdio.h>
+/**
+ ** main - Entry point
+ **
+ ** Return: 0 on success
+ **/
+int main(void)
+{
+	int n;
+
+	for(n == 0 ; n < 10 ; n++ )
+	{
+		putchar(n);
+	}
+	return (0);
+}
