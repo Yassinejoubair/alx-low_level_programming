bf8e2e53224871088f35b0d47b69898e003d2648
Yassine Joubair <yassinejoubair00@gmail.com>
2023-03-17 09:38:40 -0700

    print_alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,9 +11,11 @@ int main(void)
 
 	for (ch = 'a' ; ch <= 'z'; ++ch)
 	{
-		if ((ch == 'q' || ch == 'e') !== 0)
+		if ((ch == 'q' || ch == 'e') !== 1)
 		{
 			putchar(ch);
 		}
 	}
+	putchar('\n');
 	return (0);
+}
