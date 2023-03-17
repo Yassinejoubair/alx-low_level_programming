185899a7ef5e5c9dc1fb83a55b3f394ed4b986e8
Yassine Joubair <yassinejoubair00@gmail.com>
2023-03-17 08:29:00 -0700

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdlib.h>
+#include<stdio>
+/*main 
+ *return : 0
+ **/
+int main(void)
+{
+	char alphab;
+
+	for (alphab = 'a', alphab < 'z', alphab++)
+	{
+		putchar('alphab');
+	}
+	
+	putchar('\n');
+	
+	return(0);
+}
