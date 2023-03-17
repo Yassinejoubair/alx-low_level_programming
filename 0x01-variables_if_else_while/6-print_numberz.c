cb3df64918f7b1666b78bcfe880eda82ced5e54f
Yassine Joubair <yassinejoubair00@gmail.com>
2023-03-17 10:41:21 -0700

    NumberZ

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ * main 
+ * Use AASCII Hex Symbol
+ *  return : 0
+ **/
+int main(void)
+{
+	int i;
+
+	for (i = 48 ; i <= 57; i++)
+	{
+		putchar (i);
+	}
+
+	putchar ('\n');
+
+	return (0);
+
+}
