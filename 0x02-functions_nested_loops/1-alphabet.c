431f58165e06ea2375dfd115e625668419164407
Yassine Joubair <yassinejoubair00@gmail.com>
2023-03-21 09:34:37 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/*
+ * main - prints the alphabets
+ *
+ * 
+ */
+void print_alphabet(void)
+{
+	int c
+		for (c = 'a'; c < 'z'; ++c)
+		{
+			_putchar(ch);
+		}
+	_putchar('\n);
+}
