fd2be785544818036fce70edc69d1e0e756a2b7d
Yassine Joubair <yassinejoubair00@gmail.com>
2023-03-21 12:28:28 -0700

    Alphabetsx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * print_alphabet_x10 - function that will print the alphabet 10 times
+ *
+*/
+void print_alphabet_x10(void)
+{ 
+	int i, c;
+
+	for (i = 0; i <= 9; ++i)
+	{
+		for (c = 'a'; c <= 'z'; ++c)
+		{
+			_putchar(c);
+		}
+	}
+	_putchar('\n');
+}
