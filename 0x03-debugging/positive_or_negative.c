6d0deaa11dc4836e91b2314de8dcdaeb8491b6f0
Yassine Joubair <yassinejoubair00@gmail.com>
2023-03-22 07:56:33 -0700

    Multiple mains

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,18 @@
+int positive_or_negative(int n)
+{
+
+      if (n > 0)
+        {
+                printf("%d is %s\n", n, "positive");
+        }
+        if (n < 0)
+        {
+                printf("%d is %s\n", n, "negative");
+        }
+        if (n == 0)
+        {
+                printf("%d is %s\n", n, "zero");
+        }
+        return (0);
+}
+
