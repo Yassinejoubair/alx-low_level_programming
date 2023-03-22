736bece8c6522699263671fad5f7afdd7a7f6e63
Yassine Joubair <yassinejoubair00@gmail.com>
2023-03-22 08:46:39 -0700

    mjkk

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+        int i;
+
+        printf("Infinite loop incoming :(\n");
+
+        i = 0;
+
+        /*while (i < 10)
+        {
+                putchar(i);
+        }*/
+
+        printf("Infinite loop avoided! \\o/\n");
+
+        return (0);
+}
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+"1-main.c" [New File]                                                                                                                                             0,0-1         All
+
