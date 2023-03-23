93e48a877072c16166b42ad70d6e230e2b5df522
Yassine Joubair <yassinejoubair00@gmail.com>
2023-03-23 15:15:44 -0700

    Add _putchar

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -7,3 +9,4 @@ int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 int _putchar(char c);
+#endif
