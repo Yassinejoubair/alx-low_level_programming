bd6f97e55e037c77989132a09d95f8e736e6eb75
Yassine Joubair <yassinejoubair00@gmail.com>
2023-04-05 01:34:12 -0700

    pow

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -5,14 +5,15 @@
  * @x: int
  * @y: int
  * Return: -1 if y < 0 - int
- */  
+ */
+
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 	{
 		return (-1);
 	}
-	if(y != 0)
+	if (y != 0)
 	{
 		return (x * _pow_recursion(x, y - 1));
 	}
