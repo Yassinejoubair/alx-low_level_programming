268556d805616cec164476a3a1e9fc7bcfe9637b
Yassine Joubair <yassinejoubair00@gmail.com>
2023-04-10 07:32:09 -0700

    multipe

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: count
+ * @argv: pointer of the string
+ * Return: on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int x;
+	int y;
+	int m;
+
+	if (argc != 3)
+	{
+		printf("Error \n");
+		return (1);
+	}
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	m = x * y;
+	printf("%d \n", m);
+	return (0);
+}
