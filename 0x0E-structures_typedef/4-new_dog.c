c01da57c2e804e92532fe3c3d8f0a8b42890cbea
Yassine Joubair <yassinejoubair00@gmail.com>
2023-04-17 17:18:10 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,66 +2,49 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
 /**
- * *_strp - return a pointer to a newly allocated space in memory
- * which contains a copy of the string given as a parameter.
- * @str: string
- * Return: 0
+ * new_dog - new dog
+ * @name: name's dog
+ * @age: age's dog
+ * @owner: owner's dog
+ * Return: newdog
  */
-
-char *_strp(char *str)
-{
-	int i = 0, size = 0;
-	char *x;
-
-	if (str == NULL)
-		return (NULL);
-
-	for (; str[size] != '\0'; size++)
-		;
-	/*+1 on the size puts the end of string character*/
-	x = malloc(size * sizeof(*str) + 1);
-	if (x == 0)
-	{
-		return (NULL);
-	}
-	else
-	{
-		for (; i < size; i++)
-			x[i] = str[i];
-	}
-	return (x);
-}
-/**
- * *new_dog - create a new dog
- * @name: string for name
- * @age: integer for age
- * @owner: string for owners
- * Return: a dog or NULL
- */
-
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog *bobby;
 
-	bobby = malloc(sizeof(struct dog));
+	int i = 0, j = 0, k;
+	dog_t *bobby;
+
+	while (name[i] != '\0')
+		i++;
+	while (owner[j] != '\0')
+		j++;
+	bobby = malloc(sizeof(dog_t));
 	if (bobby == NULL)
-		return (NULL);
-	bobby->name = _strp(name);
-	if (bobby->name == NULL)
 	{
 		free(bobby);
 		return (NULL);
 	}
-	bobby->owner = _strp(owner);
-	if (bobby->owner == NULL)
+	bobby->name = malloc(i * sizeof(bobby->name));
+	if (bobby->name == NULL)
 	{
 		free(bobby->name);
 		free(bobby);
 		return (NULL);
 	}
+	for (k = 0; k <= i; k++)
+		bobby->name[k] = name[k];
 	bobby->age = age;
+	bobby->owner = malloc(j * sizeof(bobby->owner));
+	if (bobby->owner == NULL)
+	{
+		free(bobby->owner);
+		free(bobby->name);
+		free(bobby);
+		return (NULL);
+	}
+	for (k = 0; k <= j; k++)
+		bobby->owner[k] = owner[k];
 	return (bobby);
 }
