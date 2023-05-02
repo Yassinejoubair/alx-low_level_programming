a0fff4efef1682f02e685d41cc73ee89a5a57e9f
Yassine Joubair <yassinejoubair00@gmail.com>
2023-05-02 15:30:46 -0700

    v2

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,38 +1,86 @@
 #include "lists.h"
-#include <stdio.h>
+
+size_t looped(listint_t *head);
+size_t free_listint_safe(listint_t **h);
+
 /**
- * free_listint_safe - frees a linked list
- * @h: pointer to the first node in the linked list
+ * looped - Counts the number of unique nodes
+ * in a looped listint_t linked list.
+ * @head: A pointer to the head of the listint_t to check.
  *
- * Return: number of elements in the freed list
+ * Return: If the list is not looped - 0.
+ *         Otherwise - the number of unique nodes in the list.
+ */
+size_t looped(listint_t *head)
+{
+	listint_t *tp, *he;
+	size_t nod = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+	t = head->next;
+	h = (head->next)->next;
+	while (he)
+	{
+		if (tp == h)
+		{
+			tp = he;
+			while (tp != he)
+			{
+				nod++;
+				tp = tp->next;
+				he = he->next;
+			}
+			tp = tp->next;
+			while (tp != he)
+			{
+				node++;
+				tp = tp->next;
+			}
+			return (nod);
+		}
+		tp = tp->next;
+		he = (he->next)->next;
+	}
+	return (0);
+}
+
+/**
+ * free_listint_safe - Frees a listint_t list safely (ie.
+ *                     can free lists containing loops)
+ * @h: A pointer to the address of
+ *     the head of the listint_t list.
+ *
+ * Return: The size of the list that was freed.
+ *
+ * Description: The function sets the head to NULL.
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t l = 0;
-	int dif;
 	listint_t *tp;
+	size_t nod, ind;
 
-	if (!h || !*h)
-		return (0);
-
-	while (*h)
+	nod = looped(*h);
+	if (nod == 0)
 	{
-		dif = *h - (*h)->next;
-		if (dif > 0)
+		for (; h != NULL && *h != NULL; nod++)
 		{
 			tp = (*h)->next;
+			free(*h);
 			*h = tp;
-			l++;
-		}
-		else
-		{
-			*h = NULL;
-			l++;
-			break;
 		}
 	}
+	else
+	{
+		for (ind = 0; ind < nod; ind++)
+		{
+			tp = (*h)->next;
+			free(*h);
+			*h = tp;
+		}
 
-	*h = NULL;
-
-	return (l);
+		*h = NULL;
+	}
+	h = NULL;
+	return (nod);
 }
